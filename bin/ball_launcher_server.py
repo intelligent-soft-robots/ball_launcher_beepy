892fddc8ddd782e00487dd1db6e0d64e391a3905
Vincent Berenz <vberenz@tuebingen.mpg.de>
2021-11-12 16:37:53 +0100

    fixed import error in ball_launcher_server

--- bin/ball_launcher_server.py
+++ bin/ball_launcher_server.py
@@ -7,6 +7,6 @@ Starts the ball launcher server on port 5555
 from ball_launcher_beepy import BallLauncherServer
 
 if __name__ == "__main__":
-    server = ball_launcher_server.BallLauncherServer(5555)
+    server = BallLauncherServer(5555)
     server.run()
 
